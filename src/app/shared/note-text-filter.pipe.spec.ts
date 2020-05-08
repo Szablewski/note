c8b662a9ea8fa9d817134962c5e70f5cb7f83b5f
bszablewski <bartosz.szablewski123@wp.pl>
2020-05-08 20:52:23 +0200

    init

---
+++ src/app/shared/note-text-filter.pipe.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { NoteTextFilterPipe } from './note-text-filter.pipe';
+
+describe('Pipe: NoteTextFiltere', () => {
+  it('create an instance', () => {
+    let pipe = new NoteTextFilterPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
